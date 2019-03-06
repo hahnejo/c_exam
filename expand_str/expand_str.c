1e0782399d4b7b4ff4e97eb2da1793dc8f085350
hjo <hahn.jo12@gmail.com>
2019-03-05 22:46:05 -0800

    expand working

--- expand_str/expand_str.c
+++ expand_str/expand_str.c
@@ -6,35 +6,59 @@ void    ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void    expand_str(char *str)
+int     ft_strlen(char *str)
+{
+    int i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+int     ft_isspace(char *str)
 {
     int i;
-    int j;
-    int start;
-
-    start = 0;
     i = 0;
+    while (str[i])
+    {
+        if (str[i] != ' ' || str[i] != '\t')
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+// returns -1 when input is all spaces or tabs.
+#define LENGTH_ADJUST 1
+int     start_num(char *str)
+{
+    int i = 0;
     while (str[i] && (str[i] == '\t' || str[i] == ' '))
         i++;
-    start = i;
-    while (str[start])
+    return (i);
+}
+
+int     end_num(char *str)
+{
+    int end = ft_strlen(str) - 1;
+
+    while (str[end] == ' ' || str[end] == '\t')
+        end--;
+    printf("end num : %d\n", end);
+    return (end + 1);
+}
+
+void    expand_str(char *str, int start, int end)
+{
+    while (start < end)
     {
         if (str[start] != '\t' && str[start] != ' ')
-        {
             ft_putchar(str[start]);
-            // printf("put\n");
-        }
         else
         {
-            // printf("else\n");
-            while (str[start] == ' ' && str[start] == '\t')
+            while (str[start] == '\t' || str[start] == ' ')
                 start++;
-            if (str[start - 1] == ' ' && str[start - 1] == '\t')
-            {
-                ft_putchar(' ');
-                ft_putchar(' ');
-                ft_putchar(' ');
-            }
+            write(1, "   ", 3);
+            start--;
         }
         start++;
     }
@@ -42,12 +66,22 @@ void    expand_str(char *str)
 
 int     main(int argc, char **argv)
 {
+    int len;
+    int start;
+    int end;
+
     if (argc == 2)
     {
-        expand_str(argv[1]);
-        ft_putchar('\n');
+        len = ft_strlen(argv[1]);
+        start = start_num(argv[1]);
+        end = end_num(argv[1]);
+        if (ft_isspace(argv[1]) == 0)
+        {
+            ft_putchar('\n');
+            return (-1);
+        }
+        expand_str(argv[1], start, end);
     }
-    else
-        ft_putchar('\n');
+    ft_putchar('\n');
     return (0);
 }
